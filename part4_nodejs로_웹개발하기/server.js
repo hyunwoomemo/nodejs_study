4592fe8efd6f5cd62861cf6d0e9ee1cb826444e9
hyunwoomemo <archihw94@gmail.com>
2023-07-21 01:04:39 +0900

    study: 포스트 요청하기 & express.json()

---
+++ part4_nodejs로_웹개발하기/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const PORT = 4000;
+
+const Users = [
+  {
+    id: 0,
+    name: 'Hyun',
+  },
+  {
+    id: 1,
+    name: 'Kim',
+  }
+]
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`${req.method} ${req.url}`)
+  next()
+
+  const diffTime = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${diffTime}ms`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/users', (req, res) => { 
+  if (!req.body.name) {
+    res.status(400).json({
+      error: "Missing user name"
+    })
+  }
+  const newUser = {
+    name: req.body.name,
+    id: Users.length
+  }
+
+  Users.push(newUser);
+  res.json(newUser)
+})
+
+app.get('/users/:userId', (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = Users[userId];
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+})
---
+++ ~part3_node_basic/javascript/크기와_좌표.js
@@ -0,0 +1,4 @@
+// window.innerWidth / wondow.innerHeight
+
+// 현재 화면(Viewport)의 크기를 얻습니다.
+
